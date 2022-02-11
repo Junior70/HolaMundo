29c94fab7f108e426e2ef99e4d133840c7d44448
JuniorK <Junior.Cabrera@irdigital.pe>
2022-02-11 16:07:01 -0500

    Se agrego la clase Mensaje

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -2,6 +2,9 @@ package com.acme;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        //System.out.println("Hola Mundo!!");
+        Mensaje msg = new Mensaje();
+        String nombre = msg.solicitaDatos("Escribe tu Nombre: ");
+        msg.saludar(nombre);
     }
 }
